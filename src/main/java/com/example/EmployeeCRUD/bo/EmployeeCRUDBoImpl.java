6af07c559b88f38c4bab34c60adffe127853ec61
Debdyut Hajra <debdyuthajra@ymail.com>
2020-02-03 01:23:22 +0530

    feat: Prototype scope example

--- src/main/java/com/example/EmployeeCRUD/bo/EmployeeCRUDBoImpl.java
+++ src/main/java/com/example/EmployeeCRUD/bo/EmployeeCRUDBoImpl.java
@@ -5,12 +5,15 @@ import com.example.EmployeeCRUD.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
 import java.util.function.Predicate;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class EmployeeCRUDBoImpl implements EmployeeCRUDBoIfc {
 
     private static Logger log = LoggerFactory.getLogger(EmployeeCRUDBoImpl.class);
